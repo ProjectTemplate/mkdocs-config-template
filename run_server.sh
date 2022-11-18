40daf48d75f5f66e853040d0d9a09e2214f637f3
sunfeilong <sunfeilong1993@gmail.com>
2022-11-18 16:18:31 +0800

    change

--- run_server.sh
+++ run_server.sh
@@ -7,4 +7,4 @@ port=34251
 # 校验key
 auth_key="72e0013883cbc8333575c250bc0d14cd"
 
-nohup python ./server.py ${host} ${port} ${auth_key} > rebuild.log &
+nohup python /home/root/software/mkdocs/mkdocs-config-template/server.py ${host} ${port} ${auth_key} > /home/root/software/mkdocs/mkdocs-config-template/rebuild.log &
